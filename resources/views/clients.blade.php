0defbaaf77c9369cee71b3e2165e180520bc1849
core2drew <calupesandro@gmail.com>
2018-01-15 05:51:42 +0800

    Add Client Page

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -6,11 +6,21 @@
   <div class="container is-fullhd">
     <div class="columns is-marginless">
       <div class="column is-flex" style="background-image:url('images/clients/Oscar Troya.jpg')">
+        <div class="shader"></div>
         <img class="logo" src="images/clients/Oscar Troya - Logo.png"/>
       </div>
-      <div class="column" style="background-image:url('images/clients/Hasse de Moor.jpg')"></div>
-      <div class="column" style="background-image:url('images/clients/Frankie Ruscello.jpg')"></div>
-      <div class="column" style="background-image:url('images/clients/Bobby Rock.jpg')"></div>
+      <div class="column is-flex" style="background-image:url('images/clients/Hasse de Moor.jpg')">
+        <div class="shader"></div>
+        <img class="logo" src="images/clients/Hasse de Moor - Logo.png"/>
+      </div>
+      <div class="column is-flex" style="background-image:url('images/clients/Bobby Rock.jpg')">
+        <div class="shader"></div>
+        <img class="logo" src="images/clients/Bobby Rock - Logo.png"/>
+      </div>
+      <div class="column is-flex" style="background-image:url('images/clients/Frankie Ruscello.jpg')">
+        <div class="shader"></div>
+        <img class="logo" src="images/clients/Frankie Ruscello - Logo.png"/>
+      </div>
     </div>
   </div>
   @include('footer', ['class'=>'default'])
