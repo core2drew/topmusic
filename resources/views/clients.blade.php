f5924c8d9022acbbee5112c6094adb1fc0db29aa
core2drew <calupesandro@gmail.com>
2018-01-16 00:50:50 +0800

    Fix for client page artists images

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 <div id="Clients">
   @include('navbar')
-  <div class="container is-fullhd">
-    <div class="columns is-marginless">
-      <div class="column is-flex" style="background-image:url('images/clients/Oscar Troya.jpg')">
+  <div class="container is-fullhd is-fluid is-marginless">
+    <div class="columns is-multiline is-marginless">
+      <div class="column is-flex is-3" style="background-image:url('images/clients/Oscar Troya.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Oscar Troya - Logo.png"/>
       </div>
-      <div class="column is-flex" style="background-image:url('images/clients/Hasse de Moor.jpg')">
+      <div class="column is-flex is-3" style="background-image:url('images/clients/Hasse de Moor.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Hasse de Moor - Logo.png"/>
       </div>
-      <div class="column is-flex" style="background-image:url('images/clients/Bobby Rock.jpg')">
+      <div class="column is-flex is-3" style="background-image:url('images/clients/Bobby Rock.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Bobby Rock - Logo.png"/>
       </div>
-      <div class="column is-flex" style="background-image:url('images/clients/Frankie Ruscello.jpg')">
+      <div class="column is-flex is-3" style="background-image:url('images/clients/Frankie Ruscello.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Frankie Ruscello - Logo.png"/>
       </div>
