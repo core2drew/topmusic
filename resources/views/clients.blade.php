afc36f10eed5ad012a587a5a05ffef8c8467594f
core2drew <calupesandro@gmail.com>
2018-02-20 01:25:06 +0800

    Remove Oscar Troya

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -5,19 +5,19 @@
   @include('navbar')
   <div class="container is-fluid is-marginless">
     <div class="columns is-multiline is-marginless _height-initial-mobile">
-      <div class="column is-flex is-3" style="background-image:url('images/clients/Oscar Troya.jpg')">
+      <!-- <div class="column is-flex is-3" style="background-image:url('images/clients/Oscar Troya.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Oscar Troya - Logo.png"/>
-      </div>
-      <div class="column is-flex is-3" style="background-image:url('images/clients/Hasse de Moor.jpg')">
+      </div> -->
+      <div class="column is-flex is-4" style="background-image:url('images/clients/Hasse de Moor.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Hasse de Moor - Logo.png"/>
       </div>
-      <div class="column is-flex is-3" style="background-image:url('images/clients/Bobby Rock.jpg')">
+      <div class="column is-flex is-4" style="background-image:url('images/clients/Bobby Rock.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Bobby Rock - Logo.png"/>
       </div>
-      <div class="column is-flex is-3" style="background-image:url('images/clients/Frankie Ruscello.jpg')">
+      <div class="column is-flex is-4" style="background-image:url('images/clients/Frankie Ruscello.jpg')">
         <div class="shader"></div>
         <img class="logo" src="images/clients/Frankie Ruscello - Logo.png"/>
       </div>
