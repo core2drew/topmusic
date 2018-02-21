016058da2b21eacbd32d8d5a01dd4a678ddf9cb1
core2drew <calupesandro@gmail.com>
2018-02-22 00:01:34 +0800

    Moving  lato font css to head

--- resources/views/react_index.blade.php
+++ resources/views/react_index.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Top Music Management</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"/>
     <link href="css/font-icons.css" rel="stylesheet"/>
     <link href="css/bulma.css" rel="stylesheet"/>
     <link href="css/app.css" rel="stylesheet"/>
@@ -18,7 +19,7 @@
     <meta name="Keywords" content="artist management, personal branding, strategic career planning, marketing and social media, A&R consultancy, advisory, music industry, musical production, supervisor, marketing, amsterdam,">
 </head>
 <body>
-    <div id="App"></div>
+    <div id="App">Loading!</div>
     <script src="js/index.js"></script>
 </body>
 </html>
