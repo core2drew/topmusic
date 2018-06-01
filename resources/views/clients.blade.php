5a3a22539f8ad28ac3fbdea5419fd82e378dc9c6
core2drew <calupesandro@gmail.com>
2018-06-01 23:43:41 +0800

    SEO improvement

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -7,23 +7,23 @@
     <div class="columns is-multiline is-marginless _height-initial-mobile">
       <div class="column is-flex is-4-desktop is-6-tablet" style="background-image:url('images/clients/Aesty.jpg')">
         <div class="shader"></div>
-        <img class="logo" src="images/clients/Aesty - Logo.png"/>
+        <img class="logo" alt="Aesty" src="images/clients/Aesty - Logo.png"/>
       </div>
       <div class="column is-flex is-4-desktop is-6-tablet" style="background-image:url('images/clients/Aizy.jpg')">
         <div class="shader"></div>
-        <img class="logo" src="images/clients/Aizy - Logo.png"/>
+        <img class="logo" alt="Aizy" src="images/clients/Aizy - Logo.png"/>
       </div>
       <div class="column is-flex is-4-desktop is-6-tablet" style="background-image:url('images/clients/Hasse de Moor.jpg')">
         <div class="shader"></div>
-        <img class="logo" src="images/clients/Hasse de Moor - Logo.png"/>
+        <img class="logo" alt="Hasse de Moor" src="images/clients/Hasse de Moor - Logo.png"/>
       </div>
       <div class="column is-flex is-4-desktop is-6-tablet" style="background-image:url('images/clients/Bobby Rock.jpg')">
         <div class="shader"></div>
-        <img class="logo" src="images/clients/Bobby Rock - Logo.png"/>
+        <img class="logo" alt="Bobby Rock" src="images/clients/Bobby Rock - Logo.png"/>
       </div>
       <div class="column is-flex is-4-desktop is-6-tablet" style="background-image:url('images/clients/Frankie Ruscello.jpg')">
         <div class="shader"></div>
-        <img class="logo" src="images/clients/Frankie Ruscello - Logo.png"/>
+        <img class="logo" alt="Frankie Ruscello" src="images/clients/Frankie Ruscello - Logo.png"/>
       </div>
       <div class="column is-flex is-4-desktop is-6-tablet is-hidden-mobile tmm-logo" style="background-image:url('images/clients/TMM.png')">
       </div>
