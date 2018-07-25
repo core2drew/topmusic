4510d3a855afc41d0b5d251c937b6c7a5402bba3
core2drew <calupesandro@gmail.com>
2018-07-26 07:40:15 +0800

    Update message

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -7,7 +7,7 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title">
-          “We’re working on an amazing surprise, stay tunned!”
+          We’re working on an amazing surprise, stay tunned!
         </h1>
       </div>
     </div>
