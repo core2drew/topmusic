1bffdf1bfee3cc34fa225c842c3ac14de57608a2
core2drew <calupesandro@gmail.com>
2018-11-20 22:55:22 +0800

    add notif message in client page

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,115 +1,150 @@
-$(function(){
-
-  function navbarInit(){
-    let navbarBurgers = $('.navbar-burger')
+$(function() {
+  function navbarInit() {
+    let navbarBurgers = $(".navbar-burger");
     if (navbarBurgers.length > 0) {
-      navbarBurgers.on('click', function () {
-        let $this = $(this)
-        $this.toggleClass('is-active')
-        $('#navbarMenuHeroC').toggleClass('is-active')
-        $('#Navbar').toggleClass('is-active')
-      })
+      navbarBurgers.on("click", function() {
+        let $this = $(this);
+        $this.toggleClass("is-active");
+        $("#navbarMenuHeroC").toggleClass("is-active");
+        $("#Navbar").toggleClass("is-active");
+      });
     }
   }
 
   function validateEmail(mail) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    return re.test(mail)
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(mail);
   }
 
-  function contactUsInit(){
-    let notif = $("#Notif")
-    let contactUsForm = $("#ContactUsForm")
-    let isSending = false
-    let emailField = contactUsForm.find("#Email")
-    let isEmailValid = false
+  function contactUsInit() {
+    let notif = $("#Notif");
+    let contactUsForm = $("#ContactUsForm");
+    let isSending = false;
+    let emailField = contactUsForm.find("#Email");
+    let isEmailValid = false;
 
     $.ajaxSetup({
       headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      } 
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+      }
     });
 
-    let notifTimeout = function(){
-      setTimeout(function(){
-        notif.removeClass('showing').removeClass('is-danger').addClass('is-success')
-        isSending = false
-      }, 5000)
-    }
+    let notifTimeout = function() {
+      setTimeout(function() {
+        notif
+          .removeClass("showing")
+          .removeClass("is-danger")
+          .addClass("is-success");
+        isSending = false;
+      }, 5000);
+    };
 
-    notif.find(".delete").on('click', function(){
-      notif.removeClass('showing').removeClass('is-danger').addClass('is-success')
-      isSending = false
-    })
+    notif.find(".delete").on("click", function() {
+      notif
+        .removeClass("showing")
+        .removeClass("is-danger")
+        .addClass("is-success");
+      isSending = false;
+    });
 
-    contactUsForm.find('#Fullname, #Message').on('change', function(){
-      $(this).removeClass('is-danger')
-    })
+    contactUsForm.find("#Fullname, #Message").on("change", function() {
+      $(this).removeClass("is-danger");
+    });
 
-    emailField.on('change', function(e){
-      let email = e.target.value
-      let $this = $(this)
-      let label = $this.siblings('.label')
+    emailField.on("change", function(e) {
+      let email = e.target.value;
+      let $this = $(this);
+      let label = $this.siblings(".label");
 
-      if(validateEmail(email)){
-        $this.removeClass('is-danger')
-        label.removeClass('active')
-        emailField = true
-        return
+      if (validateEmail(email)) {
+        $this.removeClass("is-danger");
+        label.removeClass("active");
+        emailField = true;
+        return;
       }
-      emailField = false
-      label.addClass('active')
-      $this.addClass('is-danger')
-    })
-    
-    $('#SendMessage').on('click', function(e){
-      e.preventDefault()
-      let $this = $(this)
-      let formData = {}
-      let formErrors = []
+      emailField = false;
+      label.addClass("active");
+      $this.addClass("is-danger");
+    });
 
-      if(isSending && notif.hasClass('showing')) {
-        return false
+    $("#SendMessage").on("click", function(e) {
+      e.preventDefault();
+      let $this = $(this);
+      let formData = {};
+      let formErrors = [];
+
+      if (isSending && notif.hasClass("showing")) {
+        return false;
       }
-      
-      formErrors = contactUsForm.find('input[type=text], textarea').map(function(e, i){
-        if(!i.value.trim()) {
-          $(i).addClass('is-danger')
-          return $(i)
-        }
-        $(i).removeClass('is-danger')
-      })
 
-      if(!formErrors.length && emailField) {
-        isSending = true
-        formData = contactUsForm.serialize()
-        $this.addClass('is-loading')
-        contactUsForm.find('input[type=text], textarea').attr('disabled','disabled')
-        ga('send', 'event', 'Contact Form', 'send_form')
+      formErrors = contactUsForm
+        .find("input[type=text], textarea")
+        .map(function(e, i) {
+          if (!i.value.trim()) {
+            $(i).addClass("is-danger");
+            return $(i);
+          }
+          $(i).removeClass("is-danger");
+        });
+
+      if (!formErrors.length && emailField) {
+        isSending = true;
+        formData = contactUsForm.serialize();
+        $this.addClass("is-loading");
+        contactUsForm
+          .find("input[type=text], textarea")
+          .attr("disabled", "disabled");
+        ga("send", "event", "Contact Form", "send_form");
         $.ajax({
-          url: 'inquiry',
-          type: 'POST',
+          url: "inquiry",
+          type: "POST",
           data: formData,
           success: function(result) {
-            notif.addClass('showing')
-            $this.removeClass('is-loading')
-            contactUsForm.find('input[type=text], textarea').removeAttr('disabled')
-            contactUsForm.find('input[type=text], textarea').val("")
-            contactUsForm.find('.label').removeClass('active')
-            notifTimeout()
+            notif.addClass("showing");
+            $this.removeClass("is-loading");
+            contactUsForm
+              .find("input[type=text], textarea")
+              .removeAttr("disabled");
+            contactUsForm.find("input[type=text], textarea").val("");
+            contactUsForm.find(".label").removeClass("active");
+            notifTimeout();
           },
-          error: function(data){
-            var errors = data
-            notif.find('.header').text("Message not sent.")
-            notif.find('.content').text("Something went wrong. Please try again or message us directly to info@topmusicmanagement.com")
-            notif.addClass('showing').removeClass('is-success').addClass('is-danger')
-            notifTimeout()
+          error: function(data) {
+            var errors = data;
+            notif.find(".header").text("Message not sent.");
+            notif
+              .find(".content")
+              .text(
+                "Something went wrong. Please try again or message us directly to info@topmusicmanagement.com"
+              );
+            notif
+              .addClass("showing")
+              .removeClass("is-success")
+              .addClass("is-danger");
+            notifTimeout();
           }
         });
       }
-    })
+    });
   }
 
-  contactUsInit()
-  navbarInit()
-})
+  function infoInit() {
+    let info = $("#Info");
+    let show = info.find(".show");
+    let close = info.find(".close");
+    let notif = info.find(".notification");
+    show.on("click", function() {
+      notif.addClass("active");
+    });
+    close.on("click", function() {
+      notif.removeClass("active");
+    });
+    $(window).on("scroll", function() {
+      notif.removeClass("active");
+    });
+  }
+
+  contactUsInit();
+  navbarInit();
+  infoInit();
+});
--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -47,6 +47,28 @@
         <div class="column is-flex is-8-desktop is-hidden-mobile tmm-logo" style="background-image:url('images/clients/TMM.png')">
         </div>
     </div>
+    <div id="Info">
+      <div class="notification">
+        <button class="delete close is-large"></button>
+        Top Music Management manages solely the bookings of these artists.
+      </div>
+      <svg class="show" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 500 500" xml:space="preserve">
+        <g>
+          <rect x="135.5" y="53.5" fill="#FFFFFF" width="228" height="393"/>
+          <g>
+            <path d="M250,0C111.93,0,0,111.93,0,250c0,138.07,111.93,250,250,250c138.07,0,250-111.93,250-250
+              C500,111.93,388.07,0,250,0z M291.405,70.155c0.97-0.045,1.966-0.055,2.97,0c13.536-0.445,25.311,9.765,27.19,22.97
+              c6.045,25.354-20.885,50.77-45.624,40.625C246.396,120.036,261.28,71.62,291.405,70.155z M279.221,180.471
+              c19.189-1.22,22.539,21.11,16.559,43.75c-11.67,52.016-29.834,102.41-39.689,154.845c4.76,20.705,30.865-3.044,42.031-10.625
+              c9.494-7.55,11.145,1.915,14.374,9.846c-23.695,16.601-47.515,33.505-73.905,46.875c-24.794,15.04-45.089-8.34-38.905-34.376
+              c10.23-48.575,23.695-96.416,34.69-144.845c3.436-26.17-31.005,0.47-41.25,6.56c-8.26,10.381-12.181,2.805-15.625-6.56
+              c27.459-20.055,55.455-44.67,85.781-60.625C269.475,182.235,274.79,180.75,279.221,180.471z"/>
+          </g>
+        </g>
+      </svg>
+    </div>
+
+    
   </div>
   @include('footer', ['class'=>'default'])
 </div>
